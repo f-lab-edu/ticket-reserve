1b74343f9f7383de44a6e586e79870e238766898
jiduck1125 <gunso1125@gmail.com>
2024-05-06 15:07:49 +0900

    refactor: 테스트 공통 기능 분리 및 @DisplayName 추가

---
+++ src/test/java/com/kjh/ticketreserve/TestLanguage.java
@@ -0,0 +1,91 @@
+package com.kjh.ticketreserve;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class TestLanguage {
+
+    public static void signup(TestRestTemplate client, EmailFixture email, String password) {
+        signup(client, email.value(), password);
+    }
+
+    public static void signup(TestRestTemplate client, Credentials credentials) {
+        signup(client, credentials.email(), credentials.password());
+    }
+
+    public static void signup(TestRestTemplate client, String email, String password) {
+        Map<String, String> request = new HashMap<>();
+        request.put("email", email);
+        request.put("password", password);
+        client.postForEntity("/signup", request, Void.class);
+    }
+
+    public static String signin(
+        TestRestTemplate client,
+        EmailFixture email,
+        String password
+    ) {
+        return signin(client, email.value(), password);
+    }
+    public static String signin(
+        TestRestTemplate client,
+        Credentials credentials
+    ) {
+        return signin(client, credentials.email(), credentials.password());
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    private static String signin(TestRestTemplate client, String email, String password) {
+        Map<String, String> signRequest = new HashMap<>();
+        signRequest.put("email", email);
+        signRequest.put("password", password);
+        ResponseEntity<AccessTokenCarrier> response = client.postForEntity("/signin", signRequest, AccessTokenCarrier.class);
+        return response.getBody().accessToken();
+    }
+
+    public static <T> ResponseEntity<T> getWithToken(TestRestTemplate client, String accessToken, String path, Class<T> type) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        return client.exchange(path, HttpMethod.GET, new HttpEntity<>(headers), type);
+    }
+
+    public static <Request, Response> ResponseEntity<Response> postWithToken(
+        TestRestTemplate client,
+        String accessToken,
+        String path,
+        Request request,
+        ParameterizedTypeReference<Response> responseType) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        return client.exchange(path,
+            HttpMethod.POST,
+            new HttpEntity<>(request, headers),
+            responseType);
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    public static long createMovie(
+        TestRestTemplate client,
+        String accessToken,
+        MovieRequest movieRequest) {
+
+        ResponseEntity<MovieResponse> response = postWithToken(
+            client,
+            accessToken,
+            "/admin/movie",
+            movieRequest,
+            new ParameterizedTypeReference<>() {
+            });
+
+        return response.getBody().id();
+    }
+}
--- src/test/java/com/kjh/ticketreserve/user/GetTests.java
+++ src/test/java/com/kjh/ticketreserve/user/GetTests.java
@@ -1,25 +1,20 @@
 package com.kjh.ticketreserve.user;
 
-import com.kjh.ticketreserve.AccessTokenCarrier;
 import com.kjh.ticketreserve.AutoDomainSource;
 import com.kjh.ticketreserve.EmailFixture;
 import com.kjh.ticketreserve.UserInfo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
+import static com.kjh.ticketreserve.TestLanguage.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("GET /user/my/info")
 public class GetTests {
 
     @ParameterizedTest
@@ -31,12 +26,10 @@ public class GetTests {
     ) {
         // Arrange
         signup(client, email, password);
-        String accessToken = Objects.requireNonNull(signin(client, email, password, AccessTokenCarrier.class)
-            .getBody()).accessToken();
+        String accessToken = signin(client, email, password);
 
         // Act
-        ResponseEntity<UserInfo> response = client.exchange("/user/my/info", HttpMethod.GET,
-            new HttpEntity<>(getAuthorizationHeader(accessToken)), UserInfo.class);
+        ResponseEntity<UserInfo> response = getWithToken(client, accessToken, "/user/my/info", UserInfo.class);
 
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(200);
@@ -51,39 +44,12 @@ public class GetTests {
     ) {
         // Arrange
         signup(client, email, password);
-        String accessToken = Objects.requireNonNull(signin(client, email, password, AccessTokenCarrier.class)
-            .getBody()).accessToken();
+        String accessToken = signin(client, email, password);
 
         // Act
-        ResponseEntity<UserInfo> response = client.exchange("/user/my/info", HttpMethod.GET,
-            new HttpEntity<>(getAuthorizationHeader(accessToken)), UserInfo.class);
+        ResponseEntity<UserInfo> response = getWithToken(client, accessToken, "/user/my/info", UserInfo.class);
 
         // Assert
         assertThat(response.getBody()).hasFieldOrPropertyWithValue("email", email.value());
-    }
-
-    private static HttpHeaders getAuthorizationHeader(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        return headers;
-    }
-
-    private static void signup(TestRestTemplate client, EmailFixture email, String password) {
-        Map<String, String> request = new HashMap<>();
-        request.put("email", email.value());
-        request.put("password", password);
-        client.postForEntity("/signup", request, Void.class);
-    }
-
-    private static <T> ResponseEntity<T> signin(
-        TestRestTemplate client,
-        EmailFixture email,
-        String password,
-        Class<T> responseType
-    ) {
-        Map<String, String> signRequest = new HashMap<>();
-        signRequest.put("email", email.value());
-        signRequest.put("password", password);
-        return client.postForEntity("/signin", signRequest, responseType);
     }
 }
