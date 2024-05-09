11ccdea48b0630f87e7191be6fa1ddb6881d7da1
jiduck1125 <gunso1125@gmail.com>
2024-05-09 12:12:08 +0900

    fix: 유틸 클래스에 private 생성자, final 추가
    Resolves #4

--- src/main/java/com/kjh/ticketreserve/jpa/QuerydslPredicateUtils.java
+++ src/main/java/com/kjh/ticketreserve/jpa/QuerydslPredicateUtils.java
@@ -1,10 +1,13 @@
 package com.kjh.ticketreserve.jpa;
 
 import com.querydsl.core.types.Predicate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.function.Function;
 
-public class QuerydslPredicateUtils {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class QuerydslPredicateUtils {
 
     public static <T> Predicate ifNullNone(Function<T, Predicate> expression, T value) {
         if (value == null) {
